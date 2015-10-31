7a5063495039a158c0c9b1102b2c67345c1f5f6c
mlee0330 <michaelyurilee@gmail.com>
2015-10-31 16:33:03 -0700

    Implemented models for server

--- server/models/index.js
+++ server/models/index.js
@@ -2,17 +2,49 @@ var db = require('../db');
 
 
 
-
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      var queryString = 'SELECT messages.msgTxt, messages.objectID, messages.roomname FROM messages \
+                         LEFT OUTER JOIN users ON (messages.userID = users.userID)';
+      db.query(queryString, function(err, results){
+        if(err){
+          console.log('There was an error retrieving information from the database.');
+        }
+        callback(results);
+      });
+    }, // a function which produces all the messages
+    post: function (params, callback) {
+      var queryString = 'INSERT INTO messages (userID, msgTxt, roomname) VALUES (?, ?, ?)';
+      db.query(queryString, params, function(err, results){
+        if(err){
+          console.log('There was an error adding information to the database.');
+        }
+        callback(results);
+      });
+    }
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      var queryString = 'SELECT * FROM users';
+      db.query(queryString, function(err, results){
+        if(err){
+          console.log('There was an error retrieving information from the database.');
+        }
+        callback(results);
+      });
+    },
+    post: function (params, callback) {
+      var queryString = 'INSERT INTO users (username) VALUES (?)';
+      db.query(queryString, function(err, results){
+        if(err){
+          console.log('There was an error adding information to the database.');
+        }
+        callback(results);
+      });
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,47 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  userID int(10),
+  msgTxt varchar(255),
+  objectID int(20) NOT NULL AUTO_INCREMENT,
+  timeSent timestamp,
+  roomname varchar(20),
+  PRIMARY KEY (objectID) 
 );
+
+CREATE TABLE users (
+  userID int(10) NOT NULL AUTO_INCREMENT,
+  username varchar(16),
+  PRIMARY KEY (userID)
+);
+
+CREATE TABLE friends (
+  userID int(10),
+  friendID int(10),
+  PRIMARY KEY (userID)
+);
+
+  /* Describe your table here.*/
+  /* text */
+  /* objectID */
+  /* timeSent */
+  /* roomname */
 
 /* Create other tables and define schemas for them here! */
 
+/* users */
+-- 1mike
+-- 2kristine
+-- 3bob
+-- 4tim
 
-
+-- friendsReltn
+-- 1,2
+-- 1,4
+-- 2,3
+-- 2,4
+-- 4,1
+-- 4,2
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
